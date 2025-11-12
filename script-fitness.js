8432a6fb42e1edab02a10ebf8297ad716cf283b6
jmdavis815-web <jmdavis815@gmail.com>
2025-11-12 17:45:17 -0600

    Add files via upload

--- script-fitness.js
+++ script-fitness.js
@@ -95,7 +95,7 @@ setGoalBtn.addEventListener("click", function() {
   }
   goal = Math.min(Math.max(val, 1), 999);
   total = 0;
-  outputEl.textContent = `Goal: ${goal} min(s). Log some minutes!`;
+  outputEl.textContent = `Goal set ${goal} min(s).  Lets log some minutes!`;
   renderStatus();
   saveState();
 });
